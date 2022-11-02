1699dfd0f030dda766752eb429719a386c56616c
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-11-02 17:42:24 +0530

    Add files via upload
    added uc2

---
+++ MoodAnalyserinfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeAnalyserAssignmentuc2
+{
+    public class MoodAnalyserinfo
+    {
+        public string message { get; set; }
+        public MoodAnalyserinfo(string message)
+        {
+            this.message = message;
+        }
+        public string analyseMood(string message)
+        {
+
+            if (message.Equals(string.Empty))
+            {
+                return "Mood should not be empty";
+            }
+            else if (message.Contains("Sad"))
+            { return "Sad"; }
+            else if (message.Contains("Null"))
+            { return "Happy"; }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeAnalyserAssignmentuc2
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to reflection Demo");
+            string message = "i am happy";
+            MoodAnalyserinfo newmoodinfo = new MoodAnalyserinfo(message);
+
+            ReflectionUc2.ReflectionTest();
+            Console.ReadKey();
+        }
+    }
+}
